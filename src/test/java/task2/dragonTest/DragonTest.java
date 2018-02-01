6ef9f18d176ee41c459338a85d60c071357ebec0
Ivan Strazhevich <vanechkas@icloud.com>
2018-02-01 15:34:23 +0300

    refactoring

--- src/test/java/task2/dragonTest/DragonTest.java
+++ src/test/java/task2/dragonTest/DragonTest.java
@@ -1,12 +1,13 @@
-package task2.dragon;
+package task2.dragonTest;
 
 import task2.Viewer;
+import task2.dragon.Dragon;
 
 import java.util.Scanner;
 
 public class DragonTest {
     public static void main(String[] args) {
-        System.out.println("Enter age of the dragon: ");
+        System.out.println("Enter age of the dragonTest: ");
         int age = new Scanner(System.in).nextInt();
         Viewer.print(Dragon.countHeadsEyes(age) + "\n");
     }

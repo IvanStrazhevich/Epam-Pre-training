bb31d6c8f2aa479e36be4410b7d902b03a87be04
Ivan Strazhevich <vanechkas@icloud.com>
2018-02-02 16:53:20 +0300

    refactoring

--- src/main/java/task2/dragon/Dragon.java
+++ src/main/java/task2/dragon/Dragon.java
@@ -1,15 +1,18 @@
 package task2.dragon;
 
 public class Dragon {
+    private static final int HEADS_AT_AGE_199=600;
+    private static final int HEADS_AT_AGE_299=800;
+
     public static String countHeadsEyes(int age) {
         int heads = 0;
         int eyes = 0;
         if (age < 200) {
             heads = 3 + age * 3;
         } else if (age < 300) {
-            heads = 600 + ( age - 199 ) * 2;
+            heads = HEADS_AT_AGE_199 + ( age - 199 ) * 2;
         } else {
-            heads = 800 + ( age - 299 );
+            heads = HEADS_AT_AGE_299 + ( age - 299 );
         }
         eyes = heads * 2;
         return "Dragon of age " + age + " has " + heads + " heads and " + eyes + " eyes";
